189e635f903f033eda8650a3a6010cee4e26e9c5
Pedro Leandro <>
2022-08-09 17:59:57 -0300

    set port

--- src/config/app.js
+++ src/config/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(allowCors)
 
-app.listen(port, function() {
+app.listen(process.env.PORT || port, function() {
      console.log(`Backend, running on ${port}. `);
 })
 
