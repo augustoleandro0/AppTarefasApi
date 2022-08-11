55109891aca0ae73a77ed836370fa8bcca3b35f8
Pedro Leandro <>
2022-08-11 18:33:00 -0300

    database edit

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://deploy:admin@cluster0.mlvgbao.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://deploy:admin@cluster0.mlvgbao.mongodb.net/?retryWrites=true&w=majority/appTarefas";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("apptarefas").collection("tasks");
