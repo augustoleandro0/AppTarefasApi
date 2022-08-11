e01e3b7aa271784b7165a84ffa2e468b73b3f27a
Pedro Leandro <>
2022-08-11 20:09:08 -0300

    zdngkrsgdjcfg

--- src/config/database.js
+++ src/config/database.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb+srv://deploy:admin@cluster0.mlvgbao.mongodb.net/?retryWrites=true&w=majority/appTarefas')
+module.exports = mongoose.connect('mongodb+srv://deploy:admin@cluster0.mlvgbao.mongodb.net/?retryWrites=true&w=majority')
